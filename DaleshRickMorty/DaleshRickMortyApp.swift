b3cef7b3fd92493fc49904faf262aee99fa07f8b
Dalesh Prashar <dalesh.prash@gmail.com>
2025-01-29 09:39:00 -0600

    Initial Commit

---
+++ DaleshRickMorty/DaleshRickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaleshRickMortyApp.swift
+//  DaleshRickMorty
+//
+//  Created by DALESH PRASHAR on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DaleshRickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DaleshRickMortyTests/DaleshRickMortyTests.swift
@@ -0,0 +1,16 @@
+//
+//  DaleshRickMortyTests.swift
+//  DaleshRickMortyTests
+//
+//  Created by DALESH PRASHAR on 1/29/25.
+//
+
+import Testing
+
+struct DaleshRickMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
